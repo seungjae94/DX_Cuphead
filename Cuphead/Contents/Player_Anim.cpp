727c6ab2453a39805460dd85b764c3f131115a0c
seungjae94 이승재 <mathlife12345@gmail.com>
2024-04-11 20:15:58 +0900

    대시 리소스 수정

--- Cuphead/Contents/Player_Anim.cpp
+++ Cuphead/Contents/Player_Anim.cpp
@@ -3,20 +3,20 @@
 
 void APlayer::AnimationInit()
 {
-	Renderer->CreateAnimation(GAnimName::PlayerLeftIdle, GImageName::PlayerLeftIdle, 0.1f);
-	Renderer->CreateAnimation(GAnimName::PlayerRightIdle, GImageName::PlayerRightIdle, 0.1f);
+	Renderer->CreateAnimation(GAnimName::PlayerLeftIdle, GImageName::PlayerLeftIdle, 1/12.0f);
+	Renderer->CreateAnimation(GAnimName::PlayerRightIdle, GImageName::PlayerRightIdle, 1/12.0f);
 	Renderer->CreateAnimation(GAnimName::PlayerLeftJump, GImageName::PlayerLeftJump, 1/24.0f);
 	Renderer->CreateAnimation(GAnimName::PlayerRightJump, GImageName::PlayerRightJump, 1 / 24.0f);
-	Renderer->CreateAnimation(GAnimName::PlayerLeftDash, GImageName::PlayerLeftDash, 1 / 24.0f, false);
-	Renderer->CreateAnimation(GAnimName::PlayerRightDash, GImageName::PlayerRightDash, 1 / 24.0f, false);
-	Renderer->CreateAnimation(GAnimName::PlayerLeftShoot, GImageName::PlayerLeftShoot, 0.1f);
-	Renderer->CreateAnimation(GAnimName::PlayerRightShoot, GImageName::PlayerRightShoot, 0.1f);
+	Renderer->CreateAnimation(GAnimName::PlayerLeftDash, GImageName::PlayerLeftDash, 1 / 24.0f, false, 0, 6);
+	Renderer->CreateAnimation(GAnimName::PlayerRightDash, GImageName::PlayerRightDash, 1 / 24.0f, false, 0, 6);
+	Renderer->CreateAnimation(GAnimName::PlayerLeftShoot, GImageName::PlayerLeftShoot, 1/12.0f);
+	Renderer->CreateAnimation(GAnimName::PlayerRightShoot, GImageName::PlayerRightShoot, 1/12.0f);
 	Renderer->CreateAnimation(GAnimName::PlayerLeftRun, GImageName::PlayerLeftRun, 1/24.0f);
 	Renderer->CreateAnimation(GAnimName::PlayerRightRun, GImageName::PlayerRightRun, 1/24.0f);
-	Renderer->CreateAnimation(GAnimName::PlayerLeftRunShoot, GImageName::PlayerLeftRunShoot, 0.1f);
-	Renderer->CreateAnimation(GAnimName::PlayerRightRunShoot, GImageName::PlayerRightRunShoot, 0.1f);
-	Renderer->CreateAnimation(GAnimName::PlayerLeftSit, GImageName::PlayerLeftSit, 0.1f);
-	Renderer->CreateAnimation(GAnimName::PlayerRightSit, GImageName::PlayerRightSit, 0.1f);
+	Renderer->CreateAnimation(GAnimName::PlayerLeftRunShoot, GImageName::PlayerLeftRunShoot, 1/24.0f);
+	Renderer->CreateAnimation(GAnimName::PlayerRightRunShoot, GImageName::PlayerRightRunShoot, 1/24.0f);
+	Renderer->CreateAnimation(GAnimName::PlayerLeftSit, GImageName::PlayerLeftSit, 1/12.0f);
+	Renderer->CreateAnimation(GAnimName::PlayerRightSit, GImageName::PlayerRightSit, 1/12.0f);
 }
 
 void APlayer::ChangeAnimationIf(bool _Cond, std::string_view _IfAnim, std::string_view _ElseAnim)
